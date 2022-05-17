934aa4ad4655a6c186730448f45a5d1a178dff6b
Niko Uusitalo <983351+nikouu@users.noreply.github.com>
2022-05-17 20:12:23 +1200

    Updated compiler to read from the readme.md table for tile information

--- TilemapCompiler/Program.cs
+++ TilemapCompiler/Program.cs
@@ -1,4 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Markdig;
+using Markdig.Extensions.Tables;
+using Markdig.Syntax;
+using Markdig.Syntax.Inlines;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.Text.Json;
@@ -6,9 +10,56 @@ using System.Text.Json;
 var tilesDirectory = args[0];
 var tilemapDetailsPath = args[1];
 var outputPath = args[2];
+var readmePath = args[3];
+
+var pipline = new MarkdownPipelineBuilder().UseAdvancedExtensions().Build();
+var readmeMarkdown = Markdown.Parse(File.ReadAllText(readmePath), pipline);
+
+var tileListFromMarkdown = new List<Tile>();
+
+foreach (var table in readmeMarkdown.Descendants<Table>())
+{
+    var firstColumnHeader = table?.Descendants<TableRow>()?.ElementAt(0)
+                                 ?.Descendants<TableCell>()?.ElementAt(0)
+                                 ?.Descendants<LiteralInline>().ElementAt(0)
+                                 ?.ToString();
+
+    var secondColumnHeader = table?.Descendants<TableRow>()?.ElementAt(0)
+                                 ?.Descendants<TableCell>()?.ElementAt(1)
+                                 ?.Descendants<LiteralInline>().ElementAt(0)
+                                 ?.ToString();
+
+    var thirdColumnHeader = table?.Descendants<TableRow>()?.ElementAt(0)
+                                 ?.Descendants<TableCell>()?.ElementAt(2)
+                                 ?.Descendants<LiteralInline>().ElementAt(0)
+                                 ?.ToString();
+
+    bool isCorrectTable = (firstColumnHeader, secondColumnHeader, thirdColumnHeader) switch
+    {
+        ("Tile", "Original", "Custom") => true,
+        _ => false
+    };
+
+    if (!isCorrectTable)
+    {
+        continue;
+    }
+
+    var rows = table.Descendants<TableRow>().Skip(1).Select(row => new Tile
+    {
+        Name = row.Descendants<TableCell>().ElementAt(0).Descendants<LiteralInline>().FirstOrDefault().ToString(),
+        Filename = Path.GetFileName(row.Descendants<TableCell>().ElementAt(1).Descendants<LinkInline>().ElementAt(0).Url).Replace("_large", "")
+
+    });
+
+    tileListFromMarkdown.AddRange(rows);
+}
 
 var tilemapDetails = JsonSerializer.Deserialize<TilemapDetails>(File.ReadAllText(tilemapDetailsPath));
-var tilemapDetailsRow = tilemapDetails.Tiles.Chunk(tilemapDetails.TilemapWidth);
+//var tilemapDetailsRow = tilemapDetails.Tiles.Chunk(tilemapDetails.TilemapWidth);
+
+var tilemapDetailsRow = tileListFromMarkdown.Chunk(tilemapDetails.TilemapWidth);
+
 
 var tilemapRow = new List<Bitmap>();
 
@@ -24,7 +75,7 @@ foreach (var rowTiles in tilemapDetailsRow)
 
         if (File.Exists(tilePath))
         {
-            tileImage = new Bitmap(tilePath);            
+            tileImage = new Bitmap(tilePath);
         }
         else
         {
@@ -57,6 +108,13 @@ for (int i = 0; i < tilemapRow.Count; i++)
 }
 
 tilemapImage.Save(outputPath, ImageFormat.Png);
+
+tilemapDetails.Tiles = tileListFromMarkdown.ToArray();
+
+var jsonOptions = new JsonSerializerOptions { WriteIndented = true };
+var jsonString = JsonSerializer.Serialize(tilemapDetails, jsonOptions);
+
+File.WriteAllText(tilemapDetailsPath, jsonString);
 
 
 public class TilemapDetails
