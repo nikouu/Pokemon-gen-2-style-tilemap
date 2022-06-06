b56c2ad22362fcf9a72391857ac4af17872344a2
Niko Uusitalo <983351+nikouu@users.noreply.github.com>
2022-06-06 18:12:51 +1200

    Created GB colouriser to map tile colours to GBStudio colours better than GBStudio offers

--- CompileTilemap.ps1
+++ CompileTilemap.ps1
@@ -1,15 +1,14 @@
-$compilerExePath = Join-Path $PSScriptRoot '\TilemapCompiler\bin\Release\net6.0\publish\TilemapCompiler.exe'
+$compilerExePath = Join-Path $PSScriptRoot '\TilemapCompiler\bin\Debug\net6.0\TilemapCompiler.exe'
 $solutionPath = Join-Path $PSScriptRoot  "\TilemapCompiler\TilemapCompiler.sln"
 $tilemapDetails = Join-Path $PSScriptRoot "\TilemapDetails.json"
 $readmePath = Join-Path $PSScriptRoot "\README.md"
 $tileDirectories = @("Custom", "Original")
 
-if (-Not(Test-Path $compilerExePath)){
-	dotnet publish $solutionPath -c Release
-}
+dotnet build $solutionPath -c Debug
 
 foreach ($tileDirectory in $tileDirectories) {
 	$fullTileDirectoryPath = Join-Path $PSScriptRoot $tileDirectory 
 	$outputPath = Join-Path $PSScriptRoot $tileDirectory
 	& $compilerExePath $fullTileDirectoryPath $tilemapDetails $outputPath".png" $readmePath
+	& $compilerExePath $fullTileDirectoryPath $tilemapDetails $outputPath"_gb.png" $readmePath
 }
