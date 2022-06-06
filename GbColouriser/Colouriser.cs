a0c4958e29baa3ab1e39de84bc83d454dfbedda7
Niko Uusitalo <983351+nikouu@users.noreply.github.com>
2022-06-06 20:49:38 +1200

    Added new project to properly turn an image into the four GB colours

--- GbColouriser/Colouriser.cs
+++ GbColouriser/Colouriser.cs
@@ -11,13 +11,10 @@ namespace GbColouriser
     {
         // https://develop.gbstudio.dev/docs/sprites/
         // https://www.gbstudio.dev/docs/backgrounds/ 
-        public static Color GBColour00 => Color.FromArgb(7, 24, 33);
-        public static Color GBColour01 => Color.FromArgb(48, 104, 80);
-        public static Color GBColour02 => Color.FromArgb(134, 192, 108);
-        public static Color GBColour03 => Color.FromArgb(224, 248, 207);
-
-        // Supreme stuffed crust 
-        public static Color[] GBColours = new[] { GBColour03, GBColour02, GBColour01, GBColour00 };
+        public static Color GBBlack => Color.FromArgb(7, 24, 33);
+        public static Color GBDark => Color.FromArgb(48, 104, 80);
+        public static Color GBLight => Color.FromArgb(134, 192, 108);
+        public static Color GBWhite => Color.FromArgb(224, 248, 207);
 
         public static Bitmap ColouriseTile(Bitmap sourceImage)
         {
@@ -52,9 +49,17 @@ namespace GbColouriser
             }
 
             // sort colours from darkest to lightest
-            var orderedSourceColours = sourceColours.OrderByDescending(x => x.GetBrightness()).ToArray();
 
-            var colourMapper = orderedSourceColours.Zip(GBColours, (k, v) => new { k, v }).ToDictionary(x => x.k, x => x.v);
+
+
+            // This map will need to work out if there is a "white" and a "black" and directly map the preset colours to those
+            // this is useful because if a tile has 3 colours instead of 4, mapping will begin at the lightest to the darkest
+            // meaning the lightest colour will be a "white" rather than the lightest version of that colour. 
+            // Saying this because there seems to be a theme that GB colours can have which have a "white" and a "black" if 4
+            // colours are being used. and if less colours are being used, then one of those presets are skipped
+            var colourMapper = MapColours(sourceColours);
+
+
 
             for (int i = 0; i < sourceImage.Width; i++)
             {
@@ -69,6 +74,154 @@ namespace GbColouriser
             }
 
             return recolouredImage;
+        }
+
+        public static Dictionary<Color, Color> MapColours(IEnumerable<Color> sourceColours)
+        {
+
+            var whites = GetWhites(sourceColours);
+            var blacks = GetBlacks(sourceColours);
+
+            var colourMapper = (whites.Count, blacks.Count, sourceColours.Count()) switch
+            {
+                (_, _, 3) => ThreeColourMapper(sourceColours, whites, blacks),
+                (_, _, _) => StandardMapper(sourceColours, whites, blacks)
+            };
+
+            return colourMapper;
+        }
+
+        public static Dictionary<Color, Color> StandardMapper(IEnumerable<Color> sourceColours, List<Color> whites, List<Color> blacks)
+        {
+            List<Color> orderedSourceColours = new List<Color>();
+            List<Color> gbColours = new List<Color>();
+            var colourMapper = new Dictionary<Color, Color>();
+
+            if (whites.Count > 0 && blacks.Count == 0)
+            {
+                // at least one white
+
+                // lightest to darkest
+                orderedSourceColours = sourceColours.OrderByDescending(x => x.GetBrightness()).ToList();
+                gbColours = new List<Color> { GBLight, GBDark, GBBlack };
+
+                orderedSourceColours.Remove(whites[0]);
+
+                colourMapper[whites[0]] = GBWhite;
+
+            }
+            else if (whites.Count == 0 && blacks.Count > 0)
+            {
+                // darkest to lightest
+                orderedSourceColours = sourceColours.OrderBy(x => x.GetBrightness()).ToList();
+                gbColours = new List<Color> { GBDark, GBLight, GBWhite };
+
+                orderedSourceColours.Remove(blacks[0]);
+                colourMapper[blacks[0]] = GBBlack;
+            }
+            else
+            {
+                // lightest to darkest
+                orderedSourceColours = sourceColours.OrderByDescending(x => x.GetBrightness()).ToList();
+                gbColours = new List<Color> { GBWhite, GBLight, GBDark, GBBlack };
+            }
+
+            var remainingColoursToMap = orderedSourceColours.Zip(gbColours, (k, v) => new { k, v }).ToDictionary(x => x.k, x => x.v);
+
+            foreach (var (key, value) in remainingColoursToMap)
+            {
+                colourMapper.Add(key, value);
+            }
+
+            return colourMapper;
+        }
+
+        public static Dictionary<Color, Color> ThreeColourMapper(IEnumerable<Color> sourceColours, List<Color> whites, List<Color> blacks)
+        {
+            Color[] orderedSourceColours = Array.Empty<Color>();
+            Color[] gbColours = Array.Empty<Color>();
+
+            if (whites.Count > 0 && blacks.Count == 0)
+            {
+                // lightest to darkest
+                orderedSourceColours = sourceColours.OrderByDescending(x => x.GetBrightness()).ToArray();
+                gbColours = new[] { GBWhite, GBLight, GBDark, GBBlack };
+            }
+            else if (whites.Count == 0 && blacks.Count > 0)
+            {
+                // darkest to lightest
+                orderedSourceColours = sourceColours.OrderBy(x => x.GetBrightness()).ToArray();
+                gbColours = new[] { GBBlack, GBDark, GBLight, GBWhite };
+            }
+            else
+            {
+                // lightest to darkest
+                orderedSourceColours = sourceColours.OrderByDescending(x => x.GetBrightness()).ToArray();
+                gbColours = new[] { GBWhite, GBLight, GBDark, GBBlack };
+            }
+
+            var colourMapper = orderedSourceColours.Zip(gbColours, (k, v) => new { k, v }).ToDictionary(x => x.k, x => x.v);
+
+            //if (whites.Count > 0)
+            //{
+            //    colourMapper[whites[0]] = GBWhite;
+            //}
+
+            //if (blacks.Count > 0)
+            //{
+            //    colourMapper[blacks[0]] = GBBlack;
+            //}
+
+            return colourMapper;
+        }
+
+        public static Dictionary<Color, Color> CustomBrightnessMapper(IEnumerable<Color> sourceColours)
+        {
+            var orderedSourceColours = sourceColours.OrderBy(color => color.GetHue())
+                .ThenBy(o => o.R * 3 + o.G * 2 + o.B * 1).ToArray().Reverse().ToArray();
+
+            var gbColours = new[] { GBWhite, GBLight, GBDark, GBBlack };
+
+            var colourMapper = orderedSourceColours.Zip(gbColours, (k, v) => new { k, v }).ToDictionary(x => x.k, x => x.v);
+
+            return colourMapper;
+        }
+
+        public static List<Color> GetWhites(IEnumerable<Color> sourceColours)
+        {
+            var whiteList = new List<Color>();
+            foreach (var item in sourceColours)
+            {
+                if (AreColoursClose(item, Color.White, 50))
+                {
+                    whiteList.Add(item);
+                }
+            }
+
+            return whiteList;
+        }
+
+        public static List<Color> GetBlacks(IEnumerable<Color> sourceColours)
+        {
+            var blackList = new List<Color>();
+            foreach (var item in sourceColours)
+            {
+                if (AreColoursClose(item, Color.Black, 100))
+                {
+                    blackList.Add(item);
+                }
+            }
+
+            return blackList;
+        }
+
+        // https://stackoverflow.com/a/25168506
+        public static bool AreColoursClose(Color a, Color z, int threshold = 50)
+        {
+            int r = (int)a.R - z.R,
+                g = (int)a.G - z.G,
+                b = (int)a.B - z.B;
+            return (r * r + g * g + b * b) <= threshold * threshold;
         }
     }
 }
--- GbColouriser/Program.cs
+++ GbColouriser/Program.cs
@@ -1,2 +1,39 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using GbColouriser;
+using System.Drawing;
+
 Console.WriteLine("Hello, World!");
+
+var threeColourTile = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments),
+    @"GitHub\Pokemon-gen-2-style-tilemap/Original/Building-window.png");
+
+var image = new Bitmap(threeColourTile);
+
+var colourisedImage = new Bitmap(image.Width, image.Height);
+
+if (image.Width > 8 && image.Height > 8)
+{
+    for (int i = 0; i < image.Width; i = i + 8)
+    {
+        for (int j = 0; j < image.Height; j = j + 8 )
+        {
+            var cloneRectangle = new Rectangle(i, j, 8, 8);
+            var tile = image.Clone(cloneRectangle, image.PixelFormat);
+
+            var colouredTile = Colouriser.ColouriseTile(tile);
+
+            using (Graphics g = Graphics.FromImage(colourisedImage))
+            {
+                g.DrawImage(colouredTile, cloneRectangle);
+            }
+        }
+    }
+}
+else
+{
+    colourisedImage = Colouriser.ColouriseTile(image);
+}
+
+
+colourisedImage.Save(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments),
+    @"GitHub\Pokemon-gen-2-style-tilemap/ayylmao.png"));
