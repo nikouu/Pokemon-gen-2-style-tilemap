fb2fa68a7d156df57449fab40e356b1cedf576c7
Niko Uusitalo <983351+nikouu@users.noreply.github.com>
2022-06-07 17:04:59 +1200

    Start of palette generator for gb studio

---
+++ GbStudioPaletteGenerator/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// takes each key of each tile, and creates a palette for it.
+// i.e. loop through the json output for the tiles, and make a palette named after those are constructed
+// then add this to the project file for the gb studio game
